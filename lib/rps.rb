efdd354a74e08d5b9e55f9b2b7c62ed8ba37ce55
Mishal Islam <mishal.islam01@gmail.com>
2015-02-03 17:06:28 +0000

    game can declare winner

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,3 +1,22 @@
 class RockPaperScissors
 
+  attr_reader :players
+  attr_accessor :rules
+
+  def initialize
+    @players = []
+    @rules =  {rock: :paper, paper: :scissors, scissors: :rock}
+  end
+
+  def add_player(player)
+    @players << player unless @players.count >= 2
+  end
+
+  def declare_winner
+    if @rules[@players.first.choice.to_sym] === @players.last.choice.to_sym
+      return @players.first.name
+    end
+      return @players.last.name
+  end
+
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,11 +1,49 @@
 require 'rps'
 
 describe RockPaperScissors do
-  
+
+  let(:game)    {RockPaperScissors.new}
+  let(:player1) {double :player, choice: "rock", name: "mishal"}
+  let(:player2) {double :player, choice: "scissors", name: "computer"}
+  let(:player3) {double :player, choice: "scissors", name: "mishal"}
+  let(:player4) {double :player, choice: "rock", name: "computer"}
+
+  it 'should have no players when initialized' do
+    expect(game.players).to eq []
+  end
+
+  it 'can add a player to the game' do
+    game.add_player(player1)
+    expect(game.players).to eq [player1]
+  end
+
+  it 'cannot have more than two players in a game' do
+    3.times {game.add_player(player1)}
+    expect(game.players.count).to eq 2
+  end
+
+  it 'has a default set of rules' do
+    expect(game.rules).to eq({rock: :paper, paper: :scissors, scissors: :rock})
+  end
+
+  it 'the rules of the game can be changed' do
+    game.rules = {rock: :paper, paper: :scissors}
+    expect(game.rules).to eq({rock: :paper, paper: :scissors})
+  end
+
+  it 'player 1 wins' do
+    game.add_player(player1)
+    game.add_player(player2)
+    expect(game.declare_winner).to eq "computer"
+  end
+
+  it 'player 2 wins' do
+    game.add_player(player3)
+    game.add_player(player4)
+    expect(game.declare_winner).to eq "mishal"
+  end
+
 end
 
 # GAME
-# -has no players when initialized
-# -can have a player added to the Game 
-# -can only have two players per Game
 # -a winner is declared
